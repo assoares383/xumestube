ebf85e62ca0d1686a718d8299ef82601c0d7e1ab
assoares383 <alexandressoares383@gmail.com>
2020-12-22 20:48:57 -0300

    organizing the components on the screen with CSS Grid

---
+++ src/components/SearchBar/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import "./styles.css";
+
+const SeachBar = (props) => {
+  return (
+    <div className="search-bar">
+      <h1>Search Bar</h1>
+    </div>
+  );
+};
+
+export default SeachBar;
