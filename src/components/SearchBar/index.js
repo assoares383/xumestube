34d0eac3e4086c36a0b0be5cfb1b3bff5c53ae91
assoares383 <alexandressoares383@gmail.com>
2020-12-22 21:18:12 -0300

    creating the search function for the term

--- src/components/SearchBar/index.js
+++ src/components/SearchBar/index.js
@@ -4,11 +4,23 @@ import { Segment, Input } from "semantic-ui-react";
 import "./styles.css";
 
 class SeachBar extends Component {
+  searchTerm = (e) => {
+    if (e.keyCode === 13) {
+      const term = e.target.value;
+      console.log(term);
+    }
+  };
+
   render() {
     return (
       <div className="search-bar">
         <Segment stacked>
-          <Input icon="search" size="large" placeholder="Search ..." />
+          <Input
+            icon="search"
+            onKeyDown={(e) => this.searchTerm(e)}
+            size="large"
+            placeholder="Search ..."
+          />
         </Segment>
       </div>
     );
