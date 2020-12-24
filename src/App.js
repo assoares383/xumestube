106242603284c119d10e4e7fc4f7f2db949c8dbe
assoares383 <alexandressoares383@gmail.com>
2020-12-24 17:15:17 -0300

    connecting the project to the redux with provider

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
+import { Provider } from "react-redux";
 import "./App.css";
+
+import store from "./store";
 
 import SeachBar from "./components/SearchBar";
 import VideoList from "./components/VideoList";
@@ -6,13 +9,15 @@ import VideoPlayer from "./components/VideoPlayer";
 
 function App() {
   return (
-    <div className="container">
-      <div className="content">
-        <SeachBar />
-        <VideoPlayer />
-        <VideoList />
+    <Provider store={store}>
+      <div className="container">
+        <div className="content">
+          <SeachBar />
+          <VideoList />
+          <VideoPlayer />
+        </div>
       </div>
-    </div>
+    </Provider>
   );
 }
 
